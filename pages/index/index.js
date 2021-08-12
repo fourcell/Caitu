114c0400c2f9fac045de23e86a6455fbfb6a44ff
lvjinhao <hz936302257@163.com>
2021-08-12 17:39:55 +0800

    feat: 添加搜索框架事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -51,16 +51,24 @@ Page({
     })
   },
   onLoad() {
+
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true,
       })
     }
+    this.setData({
+      search: this.search.bind(this)
+    })
   },
-
 
   search: function (value) {
     console.log(value)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
+      }, 200)
+    })
   },
 
   selectResult: function (e) {
